51ce5a044b8346fd08fdb8eb2855084eea02df31
Michael Kirk <michael.code@endoftheworl.de>
2023-08-16 11:55:16 -0700

    globbed input

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,47 @@
-use flatgeobuf::{FgbWriter, GeometryType};
-use walkdir::WalkDir;
+mod chained_reader;
+use chained_reader::ChainedReader;
 
+use flatgeobuf::{FgbWriter, GeometryType};
 use geozero::geojson::GeoJsonLineReader;
 use geozero::GeozeroDatasource;
 
 use std::fs::File;
 use std::io::BufWriter;
-use std::path::Path;
 
 type Error = Box<dyn std::error::Error>;
 type Result<T> = std::result::Result<T, Error>;
 
-mod chained_reader;
-use chained_reader::ChainedReader;
-
 fn main() -> Result<()> {
     pretty_env_logger::init_timed();
     let args: Vec<_> = std::env::args().collect();
-    let input_dir = Path::new(args.get(1).expect("required argument: <input dir>"));
+    let input_glob = args
+        .get(1)
+        .expect("required arguments: <input glob> <output path>");
+    let output_path = args
+        .get(2)
+        .expect("required argument: <input glob> <output path>");
 
-    let input_paths = WalkDir::new(input_dir)
-        .sort_by_file_name()
+    let mut input_paths: Vec<_> = glob::glob(input_glob)
         .into_iter()
+        .flatten()
         .flat_map(|entry| {
             let entry = entry.expect("valid directory entry");
             if entry.metadata().expect("valid metadata").is_dir() {
                 return None;
             }
-            Some(entry.path().to_owned())
+            Some(entry)
         })
         .collect();
+    input_paths.sort();
+    input_paths.reverse();
+
     let mut input = ChainedReader::new(input_paths);
 
-    let stem = input_dir.file_stem().expect("stemmable file name");
-
-    let output_path = format!("{stem}.fgb", stem = stem.to_string_lossy());
     let output_file = File::create(output_path)?;
     let mut output = BufWriter::new(output_file);
 
     let mut reader = GeoJsonLineReader::new(&mut input);
-    let mut writer = FgbWriter::create(&stem.to_string_lossy(), GeometryType::Point)?;
+    let mut writer = FgbWriter::create("fgb", GeometryType::Point)?;
 
     log::info!("start processing");
     reader.process(&mut writer)?;
